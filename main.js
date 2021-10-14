7def0c345ce9e6776786377a2bcdfb9f26a5b17d
aladin-jridi <aladinjridi007@gmail.com>
2021-10-14 11:33:47 +0100

    trying

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function song(){
     document.removeEventListener('click',song)
 }
 
+
 function cont(){
 var counter =60;
 setInterval(function(){    
@@ -12,6 +13,8 @@ setInterval(function(){
         document.getElementById('count').innerHTML = counter
     }if(counter===0){
         document.getElementById('count').innerHTML ='your time is over'
+         $('body').append('<audio autoplay><source src="./song/explosion-sound-effect-10-seconds.mp3" type="audio/mp3"> </audio>')
+        
     }
+   
 },1000);}
-    
